b4f308f10b19bde1d235ff06c0660043d07e5d50
Alvaro <alvaroariasrivero92@gmail.com>
2021-10-20 14:03:25 +0200

    hecho el rombo

--- script.js
+++ script.js
@@ -42,9 +42,9 @@
 // let n = 5
 // for(let i = 0; i < n; i++){
 //   if(i % 2 == 0){
-//     r = r + c1
-//   } else {
 //     r = r + c2
+//   } else {
+//     r = r + c1
 //   }
 // }console.log(r)
 
@@ -124,6 +124,36 @@
 //    ++++++
 //     ++++
 
+
+// let r = ''
+// let n = 4
+// for(let i = 0; i < n; i++){
+//   let espace = (n - 1) - i
+//   for(let j = 0; j < n*2 - 1 + i; j++){
+//     if(espace > 0){
+//       r= r + ' '
+//     } else {
+//     r=r+'*'
+//     } espace = espace - 1
+//   }
+//     console.log(r)
+//     r = ''
+// }
+// for(let k = n - 1; k > 0; k--){
+//   let espace = n - k
+//   for(let j = 0; j < k + n*2 - 2; j++){
+//     if(espace > 0){
+//       r = r + ' '
+//     } else {
+//     r=r+'*'
+//     } espace = espace - 1
+//   }
+//     console.log(r)
+//     r = ''
+// }
+
+
+
 // Escriba un programa que pida al usuario ingresar un usuario y una contraseña, y le indique si ha validado o no, debemos tener mínimo 5 usuarios en el Local Storage (cargados previamente por el programa, con formato a elegir por el alumno) y usarlos para saber si el usuario ha accedido o no.
 
 
@@ -144,4 +174,22 @@ press.addEventListener("click", (e)=>{
   localStorage.setItem(`usuario${i}`, JSON.stringify(usuario))
 })
 
+// Ten precargado en un array 20 números y ordénalo.
+
+function ordenamientoBurbuja(items) {
+  let length = items.length;
+  for(let i = 0; i < length; i++) {
+    for(let j = 0; j < (length -i -1); j++) {
+      if(items[j] > items[j+1]) {
+        let numact = items[j];
+        items[j] = items[j+1];
+        items[j+1] = numact;
+      }
+    }
+  }
+}
+
+let arr = [20, 18, 19, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+ordenamientoBurbuja(arr)
+console.log(arr)
 
